457479e6fff5297345e6953d5c346176a882bc5d
Negin Zahedi <zahedi.negin@gmail.com>
2024-05-25 14:53:41 -0400

    Initial Commit

---
+++ BiometricAuthentication-SwiftUI/BiometricAuthentication_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiometricAuthentication_SwiftUIApp.swift
+//  BiometricAuthentication-SwiftUI
+//
+//  Created by Negin Zahedi on 2024-05-25.
+//
+
+import SwiftUI
+
+@main
+struct BiometricAuthentication_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BiometricAuthentication-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BiometricAuthentication-SwiftUI
+//
+//  Created by Negin Zahedi on 2024-05-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
