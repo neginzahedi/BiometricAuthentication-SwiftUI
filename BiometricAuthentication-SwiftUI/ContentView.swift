4bb8a76ab156ce220c91426a476650c55ec85719
Negin Zahedi <zahedi.negin@gmail.com>
2024-05-25 15:08:48 -0400

    Add authentication functionality content

--- BiometricAuthentication-SwiftUI/ContentView.swift
+++ BiometricAuthentication-SwiftUI/ContentView.swift
@@ -7,17 +7,80 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct ContentView: View {
+    
+    // MARK: - Properties
+    
+    @ObservedObject var authenticationController = AuthenticationController()
+    @State private var status = ""
+    @State private var biometricAvailable = true
+    
+    // MARK: - Body
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(status)
+                .foregroundColor(biometricAvailable ? .primary : .red)
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+                .padding(.top, 100)
+                .padding(.bottom, 50)
+            
+            if !authenticationController.isLoggedIn {
+                // Display biometric authentication buttons if user is not logged in
+                if authenticationController.biometricType == .faceID {
+                    Button {
+                        authenticate()
+                    } label: {
+                        Label("Use FaceID", systemImage: "faceid")
+                    }
+                    .buttonStyle(.bordered)
+                } else if authenticationController.biometricType == .touchID {
+                    Button {
+                        authenticate()
+                    } label: {
+                        Label("Use TouchID", systemImage: "touchid")
+                    }
+                    .buttonStyle(.bordered)
+                } else {
+                    Text("No Biometric Option Available")
+                }
+            }
+            Spacer()
         }
-        .padding()
+        .onAppear(perform: checkPermission)
+    }
+    
+    // MARK: - Methods
+    
+    /// Check biometric availability and update UI accordingly.
+    func checkPermission() {
+        authenticationController.askBiometricAvailability { error in
+            if let error {
+                status = "Error: " + "\n" + error.localizedDescription
+                biometricAvailable = false
+            } else {
+                biometricAvailable = true
+            }
+        }
+    }
+    
+    /// Authenticate using biometric authentication.
+    func authenticate() {
+        authenticationController.authenticate { result in
+            switch result {
+            case .success(_):
+                status = "\n Logged In."
+            case .failure(let failure):
+                status = "Status:" + failure.localizedDescription
+            }
+        }
     }
 }
+
+// MARK: - Preview
 
 #Preview {
     ContentView()
